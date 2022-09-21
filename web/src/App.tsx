4e86a59dc8b9a8e3a768f82598509afc6b2f68a4
Ana Garcia <anagarcia.vancouver@gmail.com>
2022-09-21 09:09:18 -0700

    Add Text on Cards

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -10,29 +10,33 @@ function App() {
     <h1 className="text-6xl text-white font-black m-20 mt-16 ">
       Your <span className='text-transparent bg-nlw-gradient bg-clip-text'>duo</span> is here.
     </h1>
-    <h1 className="text-6xl text-white font-black m-20 mt-16 ">
-      Your <span className='text-transparent bg-nlw-gradient bg-clip-text'>duo</span> is here.
-    </h1>
-
+    
     <div className='grid grid-cols-6 gap-6'>
 
-      <a>
-        <img src='/game-1.png' alt=''></img>        
+      <a href='' className="relative">
+        <img src='/game-1.png' alt=''/>
+
+        <div className="w-full pt-16 pb-4 px-4 bg-game-gradient">
+
+          <strong className="font-bold text-white">League of Legends</strong>
+          <span className="text-zinc-300 text-sm ">4 Ads</span>
+
+        </div>      
       </a>
       <a>
-        <img src='/game-2.png' alt=''></img>        
+        <img src='/game-2.png' alt=''/>        
       </a>
       <a>
-        <img src='/game-3.png' alt=''></img>        
+        <img src='/game-3.png' alt=''/>        
       </a>
       <a>
-        <img src='/game-4.png' alt=''></img>        
+        <img src='/game-4.png' alt=''/>        
       </a>
       <a>
-        <img src='/game-5.png' alt=''></img>        
+        <img src='/game-5.png' alt=''/>        
       </a>
       <a>
-        <img src='/game-6.png' alt=''></img>        
+        <img src='/game-6.png' alt=''/>        
       </a>
       
     </div>
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -12,7 +12,7 @@ module.exports = {
       backgroundImage: {
         galaxy: "url('/background-galaxy.png')",
         'nlw-gradient': 'linear-gradient(88.38deg, #9572FC 17.62%, #43E7AD 52.65%, #E1D55D 98.8%)',
-
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
       }
     },
   },
