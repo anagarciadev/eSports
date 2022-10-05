fe2b390eea52950641bf9d56fdf1d4d7193e9805
Ana Garcia <anagarcia.vancouver@gmail.com>
2022-10-05 11:15:17 -0700

    Finalize BackEnd routes and Functionalities

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,16 @@
 //Importing 'express' and declaring a variable 'e'
 import express from 'express'
+import cors from 'cors'
+
 import { PrismaClient } from '@prisma/client'
+import { convertHourStringToMinutes } from './utils/convert-hour-string-to-minutes'
+import { convertMinutesToHourString } from './utils/convert-minutes-to-hour-string'
 
 //declaring a variale 'app' and adressing to the function 'e'
-const app = express();
+const app = express()
+
+app.use(express.json())
+app.use(cors())
 
 const prisma = new PrismaClient({
     log: ['query']
@@ -22,28 +29,84 @@ app.get('/games', async (request, response) => {
     })
 
     return response.json(games)
-})
+});
 
-app.post('/ads', (request, response) => {
-    return response.status(201).json([])
-})
+app.post('/games/:id/ads', async (request, response) => {
+    const gameId = request.params.id;
+    const body: any = request.body;
+
+    const ad = await prisma.ad.create ({ 
+        data: {
+            gameId,
+            name:  body.name,
+            yearsPlaying:  body.yearsPlaying,
+            discord:  body.discord,
+            weekDays:  body.weekDays.join(','),
+            hourStart: convertHourStringToMinutes (body.hourStart),
+            hourEnd:  convertHourStringToMinutes(body.hourEnd),
+            useVoiceChannel:  body.useVoiceChannel,
+
+      }  
+    })
+
+    return response.status(201).json(ad);
+});
 
 //callback function - a function passed into another function as an argument. Arguments are elements that fill parameters
-app.get('/games/:id/ads', (request, response) => {
-    return response.json([
-        { id: 1, name: 'Ad 1' },
-        { id: 2, name: 'Ad 2' },
-        { id: 3, name: 'Ad 3' },  
-        { id: 4, name: 'Ad 4' },   
-        { id: 5, name: 'Ad 5' },   
-        
-    ])
-})
+app.get('/games/:id/ads', async (request, response) => {
+
+    const gameId = request.params.id;
+
+    const ads = await prisma.ad.findMany ({
+        select: {
+            id: true,
+            name: true,
+            weekDays: true,
+            useVoiceChannel: true,
+            yearsPlaying: true,
+            hourStart: true,
+            hourEnd: true,
+
+        },
+
+        where: {
+            gameId,
+        },
+
+        orderBy: {
+            createdAt: 'desc',
+        }
+    }) 
+
+    return response.json(ads.map(ad => {
+        return {
+            ...ad,
+            weekDays: ad.weekDays.split (','),
+            hourStart: convertMinutesToHourString(ad.hourStart),
+            hourEnd: convertMinutesToHourString(ad.hourEnd),
+
+        }
+
+
+    }))
+});
 
 //callback function - a function passed into another function as an argument. Arguments are elements that fill parameters
-app.get('/ads/:id/discord', (request, response) => {
-    return response.json([])
-})
+app.get('/ads/:id/discord', async (request, response) => {
+    const adId = request.params.id;
 
+    const ad = await prisma.ad.findUniqueOrThrow({
+        select: {
+            discord: true,
+        },
+        where: {
+            id: adId,
+        }
+    })
+
+    return response.json({
+        discord: ad.discord,
+    })
+});
 
 app.listen(3333)
