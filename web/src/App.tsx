1f86249890cb881df0419404ea286250d2ac7d54
Ana Garcia <anagarcia.vancouver@gmail.com>
2022-10-07 15:54:21 -0700

    Create Dialog Banner

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,6 @@
 //Components and Properties
 import { Key, useEffect, useState } from 'react';
-import {MagnifyingGlassPlus } from 'phosphor-react';
+import {GameController, MagnifyingGlassPlus } from 'phosphor-react';
 import * as Dialog from '@radix-ui/react-dialog';
 
 import './styles/main.css';
@@ -58,12 +58,74 @@ function App() {
       <CreateAdBanner/>
 
       <Dialog.Portal>
-        <Dialog.Overlay/>
-        <Dialog.Content>
-          <Dialog.Title>Create an Ad</Dialog.Title> 
+        <Dialog.Overlay className='bg-black/60 inset-0 fixed' />
+
+        <Dialog.Content className='fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[540px] shadow-lg shadow-black/25'>
+          <Dialog.Title className='text-3xl font-black'>Create an Ad</Dialog.Title> 
+          
           <Dialog.Content>
-            Create an Ad
+            <form>
+
+              <div>
+                <label htmlFor='game'>Which game?</label>
+                <input id='game' placeholder='Select the game you wanna play'/>
+              </div>
+
+              <div>
+                <label htmlFor='name'>Your name or nickname</label>
+                <input id='name' placeholder='How they call you when you play?'/>
+              </div>
+
+              <div>
+
+                <div>
+                  <label htmlFor='yearsPlaying'>For how many years do you play?</label>
+                  <input id='yearsPlaying' type='number' placeholder='No problem if its ZERO'/>
+                </div>
+
+                <div>
+                  <label htmlFor='discord'>What is your Discord?</label>
+                  <input id='discord'type='text' placeholder='User#0000'/>
+                </div>
+
+              </div>
+
+              <div>
+
+                <div>
+                  <label htmlFor='weekDays'>When do you like to play?</label>
+                </div>
+
+                <div>
+                  <label htmlFor='hourStart'>At what time?</label>
+                  <div>
+                    <input id='hourStart' type='time' placeholder='From'></input>
+                    <input id='hourEnd' type='time' placeholder='To'></input>
+
+                  </div>
+                </div>
+
+              </div>
+
+              <div>
+                <input type="checkbox" />
+                I usually connect to the voice channel
+              </div>
+
+              <footer>
+                <button>Cancel</button>
+                <button type='submit'>
+                  <GameController />
+                  Find Duo
+                  </button>
+              </footer>
+
+
+
+            </form>
+          
           </Dialog.Content>
+        
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
